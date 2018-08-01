15261c3d9fe01d9704991986819781fbbce17891
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-02 08:45:15 +1200

    Exercise 2

---
+++ Person/Person/Person1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person1
+    {
+        string fname;
+        string lname;
+
+        public string Fname
+        {
+            get
+            {
+                return fname;
+            }
+            set
+            {
+                fname = value;
+            }
+        }
+        public string Lname
+        {
+            get
+            {
+                return lname;
+            }
+            set
+            {
+                lname = value;
+            }
+        }
+
+        public Person1(string _fname, string _lname)
+        {
+            Fname = _fname;
+            Lname = _lname;
+        } 
+   
+    }
+}
+
---
+++ Person/Person/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person1 employee1 = new Person1("Jacob", "Er");
+            employee1.Fname = "Jacob";
+            Console.WriteLine(employee1.Fname);
+            Console.WriteLine(employee1.Lname);
+        }
+    }
+}
